8f058d6229b946bffda2ea270d2c77a6433d2c3c
Bartosz Kot <bartosz.kot.00@gmail.com>
2021-11-19 15:11:42 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// This app checks if string inputted by user is a palindrome or not.
+        Scanner myScan = new Scanner(System.in);
+        System.out.print("Input the word to check: ");
+        String word = myScan.nextLine();
+        String revWord = new StringBuffer(word).reverse().toString();
+
+        if(word.equalsIgnoreCase(revWord))
+        {
+            System.out.println("Word '" + word + "' is a palindrome.");
+        }
+        else
+        {
+            System.out.println("Word " + word + " is NOT a palindrome.");
+        }
+    }
+}
